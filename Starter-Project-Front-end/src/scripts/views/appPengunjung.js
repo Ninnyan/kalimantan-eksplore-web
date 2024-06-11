5a07fde600227aff3c9f54c41081f020aa612684
Riskypr <103392320+Riskypr@users.noreply.github.com>
2024-06-11 13:52:04 +0700

    Add files via upload
    first commit

---
+++ Starter-Project-Front-end/src/scripts/views/appPengunjung.js
@@ -0,0 +1,9 @@
+import '../component/navbar.js'
+import '../component/footer.js'
+import { filterCategory } from './pages/destinasiData.js';
+import { moveSlide } from './pages/rekomendasiData.js';
+import './pages/script.js';
+
+
+window.filterCategory = filterCategory;
+window.moveSlide = moveSlide;
